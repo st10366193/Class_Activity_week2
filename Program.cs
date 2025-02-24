a891ed5c082d8b8b55d360e6da932e8f9362aa81
Katlego Makhuba <128791831+st10366193@users.noreply.github.com>
2025-02-24 14:32:11 +0000

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Class_Activity_week2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Please enter a number: ");
+            string input = Console.ReadLine();
+
+           
+            int number;
+            if (Int32.TryParse(input, out number))
+            {
+                // Use a loop to print all numbers from 0 to the entered number
+                for (int i = 0; i <= number; i++)
+                {
+                    Console.WriteLine(i);
+                }
+            }
+            else
+            {
+                Console.WriteLine("Invalid input. Please enter a valid number.");
+            
+        }
+            }
+        
+}
+}
